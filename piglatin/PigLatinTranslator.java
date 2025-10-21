0ac91924e2b8b34ecd9b7fe7cb9be4326bf08291
TristenLiu5 <electricemerald0825@gmail.com>
2025-10-21 22:16:44 +0000

    improved a lot of my scanner code with new bits of code

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -1,5 +1,8 @@
 package piglatin;
 
+import java.util.Scanner;
+
+
 public class PigLatinTranslator {
     public static Book translate(Book input) {
         Book translatedBook = new Book();
@@ -13,18 +16,35 @@ public class PigLatinTranslator {
         return translatedBook;
     }
 
+
     public static String translate(String input) {
         System.out.println("  -> translate('" + input + "')");
 
         String result = "";
 
-        // TO DO: translate a string input, store in result.
-        // The input to this function could be any English string.
-        // It may be made up of many words.
-        // This method must call translateWord once for each word in the string.
+        Scanner sc = new Scanner(input);
+        while (sc.hasNext())
+    {
+        String word = input.next();
+    }
+
         result = translateWord(input);
 
         return result;
+    }
+
+
+    public static boolean vowel(String letter)
+    {
+        if (letter.length() == 1)
+        {
+            String vowelBank = "aeiouy";
+            if (vowelBank.indexOf(letter) != -1)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     private static String translateWord(String input) {
@@ -32,23 +52,19 @@ public class PigLatinTranslator {
 
         String result = "";
 
-
-        if ((input.indexOf("a")) == (0) || (input.indexOf("e")) == (0) || (input.indexOf("i")) == (0) || (input.indexOf("o")) == (0) || (input.indexOf("u")) == (0) || (input.indexOf("y")) == (0) || (input.indexOf("A")) == (0) || (input.indexOf("E")) == (0) || (input.indexOf("I")) == (0) || (input.indexOf("O")) == (0) || (input.indexOf("U")) == (0) || (input.indexOf("Y")) == (0)) 
+        for (int i = 0; i < input.length(); i++)
         {
-            result = (input + "ay");
+            String current = input.substring(i, i+1);
+            if (vowel(current))
+            {
+                //if (isUpperCase(input.substring(0)) == false)
+                //{
+                    result = (input.substring(0,i) + input.substring(i) + "ay");
+                //}
+                break;
+            }
         }
-        else if ((input.indexOf("a") > 0) && ((input.indexOf("e")) > (input.indexOf("a")) || (input.indexOf("e")) == -1) && ((input.indexOf("i")) > (input.indexOf("a")) || (input.indexOf("i")) == -1) && ((input.indexOf("o")) > (input.indexOf("a")) || (input.indexOf("o")) == -1) && ((input.indexOf("u")) > (input.indexOf("a")) || (input.indexOf("u")) == -1) && ((input.indexOf("y")) > (input.indexOf("a")) || (input.indexOf("y")) == -1))
-        {
-            int index = input.indexOf("a");
-
-            result = (input.substring(index) + input.substring(0,index) + "ay");
-        }
-        else if ((input.indexOf("e") > 0) && ((input.indexOf("e")) > (input.indexOf("a")) || (input.indexOf("a")) == -1) && ((input.indexOf("e")) > (input.indexOf("i")) || (input.indexOf("i")) == -1) && ((input.indexOf("e")) > (input.indexOf("o")) || (input.indexOf("o")) == -1) && ((input.indexOf("e")) > (input.indexOf("u")) || (input.indexOf("u")) == -1) && ((input.indexOf("e")) > (input.indexOf("y")) || (input.indexOf("y")) == -1))
-        {
-            int index = input.indexOf("a");
-
-            result = (input.substring(index) + input.substring(0,index) + "ay");
-        }
+        
         
 
         return result;
