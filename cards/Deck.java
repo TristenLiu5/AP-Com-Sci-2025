3a3461750d9309ca42ad33ac320c3cdcd911a911
TristenLiu5 <electricemerald0825@gmail.com>
2025-11-14 23:16:47 +0000

    started working on it

--- cards/Deck.java
+++ cards/Deck.java
@@ -2,5 +2,17 @@ package cards;
 
 public class Deck {
 
+    private int suit;
+    private int value;
+
+    private static String[] SUITS = {"♦️", "♥️" , "♣️", "♠️"};
+    private static String[] VALUES = {"A", "2" , "3", "4","5", "6" , "7", "8","9", "10" , "J", "Q", "K"};
+
+    public Card(int suit, int value)
+    {
+        
+    }
+
+
 
 }
