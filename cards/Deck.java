75907b3b02419f7f67f29035e61fca155e607e77
TristenLiu5 <electricemerald0825@gmail.com>
2025-11-17 22:47:49 +0000

    added aa bit more code

--- cards/Deck.java
+++ cards/Deck.java
@@ -8,10 +8,21 @@ public class Deck {
     private static String[] SUITS = {"♦️", "♥️" , "♣️", "♠️"};
     private static String[] VALUES = {"A", "2" , "3", "4","5", "6" , "7", "8","9", "10" , "J", "Q", "K"};
 
-    public Card(int suit, int value)
+    public void Card(int suit, int value)
     {
-        
+        this.suit = suit;
+        this.value = value;
     }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public String toString()
+    {
+        return "" + SUITS[suit] + VALUES[value];
+    } 
 
 
 
