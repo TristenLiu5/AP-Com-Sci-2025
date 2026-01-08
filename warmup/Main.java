2e5130da5431a9cde48fc9ceafe29718de0b6889
TristenLiu5 <electricemerald0825@gmail.com>
2026-01-08 23:10:58 +0000

    Finished 2 things, the Multiples of 3 and 5, and Even Fibonacchi Numbers

--- warmup/Main.java
+++ warmup/Main.java
@@ -5,5 +5,46 @@ public class Main {
     public static void main(String[] args) {
         // Your code here
 
+        int number = 0;
+        System.out.println(number);
+
+        for (int i = 0; i <= 1000; i++)
+        {
+            if (i%3 == 0)
+            {
+                number += i;
+                System.out.println(number);
+            }
+            else if (i%5 == 0)
+            {
+                number += i;
+                System.out.println(number);
+            }
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+            int first = 0;
+            int second = 1;
+            int third = second;
+            int sum = 0;
+
+            while ((second <= 4000000 && first <= 4000000))
+            {
+                third = second;
+                second = first + second; //1
+                first = third;
+                System.out.println(first + "," + second);
+
+                if (second % 2 == 0)
+                {
+                    sum += second;
+                    System.out.println(sum);
+                }
+            }
     }
+
+
+
 }
