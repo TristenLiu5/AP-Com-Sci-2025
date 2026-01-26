a8aa6d0c4afb7cf0cb434a5a5130005ea1e87d8b
TristenLiu5 <electricemerald0825@gmail.com>
2026-01-26 23:16:58 +0000

    started work on it

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -4,5 +4,12 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int temp;
+
+        for (int i = 0; i <= input.length - 1; i++)
+        {
+            if (input[i] = )
+        }
+
     }
 }
