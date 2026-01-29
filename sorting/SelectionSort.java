3aff95159b8d0c03412cda4025cdc78fbde347b8
TristenLiu5 <electricemerald0825@gmail.com>
2026-01-29 00:38:06 +0000

    finished selection

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -5,7 +5,19 @@ public class SelectionSort implements Sorter {
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
 
+        for (int i = 0; i <= input.length - 1; i++)
+        {
+            int temp = 100;
 
-        
+            for (int j = i; j <= input.length - 1; j++)
+
+            if (temp > input[j])
+            {
+                temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+            }
+        }
+
     }
 }
