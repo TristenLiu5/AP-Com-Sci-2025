81a926bb2212c1946a64639d93fde7ccfc912d77
TristenLiu5 <electricemerald0825@gmail.com>
2026-03-03 21:57:23 +0000

    I FINALLY MANAGED TO GET THE CODE WORKING, IT WAS LITERALLY ONE THING
    after i did all the perimeteer and area bc they are pretty easy

--- polygon/IrregularPolygon.java
+++ polygon/IrregularPolygon.java
@@ -11,29 +11,60 @@ public class IrregularPolygon {
     public IrregularPolygon() {
 
 
+
     }
 
     // public methods
     public void add(Point2D.Double aPoint)
     {
-        Point2D.Double(aPoint, 0.0);
+        myPolygon.add(aPoint);
         // TODO: Add a point to the IrregularPolygon.
     }
 
     public double perimeter() {
         // TODO: Calculate the perimeter.
-        myPolygon.getX();
-        myPolygon.getY();
+        double perimeter = 0.0;
+        int size = myPolygon.size();
 
-        //for (int i = 0; i < )
+        if(size > 1) {
+            for(int i = 0; i < size; i++) {
+                Point2D.Double p1 = myPolygon.get(i);
+                Point2D.Double p2 = myPolygon.get((i + 1) % size);
 
-        return 3.14;
+                perimeter += (Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2)));
+
+            }
+        }
+
+
+         return perimeter;
     }
 
     public double area() {
         // TODO: Calculate the area.
+        int size = myPolygon.size();
         Double area = 0.0;
-        return area;
+
+        for(int i = 0; i < size; i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % size);
+
+            area += (p2.getX() * p1.getY());
+
+
+        }
+        for(int j = 0; j < size; j++) {
+            Point2D.Double p1 = myPolygon.get(j);
+            Point2D.Double p2 = myPolygon.get((j + 1) % size);
+
+            area -= (p2.getY() * p1.getX());
+
+
+        }
+
+        area /= 2;
+
+        return Math.abs(area);
     }
 
     public void draw()
@@ -45,7 +76,7 @@ public class IrregularPolygon {
             // DrawingTool myDrawingTool = new DrawingTool(new SketchPad(500, 500));
             // myDrawingTool.move(TestSuite.singlePoint.getX(0), 50);
             // myDrawingTool.move(100,0);
-            drawLine(0, 0);
+            //drawLine(0, 0);
 
 
         } catch (java.awt.HeadlessException e) {
